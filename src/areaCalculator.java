4567002eca3860cd5f937e99fdaf30536260746d
paragip <pal.paragi@gmail.com>
2022-05-30 22:12:23 +0200

    area calculator circle, square

---
+++ src/areaCalculator.java
@@ -0,0 +1,22 @@
+public class areaCalculator {
+    public static void main(String[] args) {
+        System.out.println(area(-5, 4));
+    }
+
+    public static double area(double radius){
+        if (radius < 0) {
+            return -1;
+        }
+        return (radius * radius * Math.PI);
+    }
+
+    public static double area(double x, double y){
+        if (x < 0 || y < 0) {
+            return -1;
+        }
+        return (x * y);
+    }
+
+}
+
+
